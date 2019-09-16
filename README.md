89089112da0e81b1ed72eb63e86ef3174be52934
1187022494 <1187022494@qq.com>
2019-09-16 23:18:33 +0800

    add

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 #### 这里是Dev分支
+### 文件目录结构
+pages   页面
+components  组件
+style   样式
+utils   公用的库或者插件
+router  路由文件
+store   全局状态管理文件
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'antd/dist/antd.css';
+import axios from 'utils/axios.js'
 
+React.Component.prototype.$axios = axios
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
