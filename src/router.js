b7b438c9c826b8e013ddae47744d2e6fe7ce196e
biubiubiu-bei <you@example.com>
2019-09-17 20:53:20 +0800

    router

--- src/router.js
+++ src/router.js
@@ -1,32 +1,33 @@
- import React,{Component} from 'react'
- import {HashRouter,Route,Switch,Redirect} from 'react-router-dom'
- import App from './App'
- import Login from 'pages/login'
- import Admin from 'pages/admin'
- import Food from 'pages/food'
- import User from 'pages/user'
+import React,{Component} from 'react'
+import {HashRouter,Route,Switch,Redirect} from 'react-router-dom'
 
- class RootRouter extends Component{
-     render(){
-         return(
-             <App>
-                 <HashRouter>
+import App from './App' 
+import Login from 'pages/login'
+import Admin from 'pages/admin'
+import Food from 'pages/food'
+import User from 'pages/user'
+class RootRouter extends Component{
+    render(){
+        return(
+            <App>
+                <HashRouter>
                     <Switch>
                         <Redirect exact from='/' to='/admin'></Redirect>
-                        <Route path = '/admin' render={()=>{
-                            
+                        <Route path='/admin' render={()=>{
                             return(
-                              <Admin>
-                                  <Route path = '/admin/user' component={User}></Route>
-                                  <Route path = '/admin/food' component={Food}></Route>
-                              </Admin>  
+                                <Admin>
+                                      <Route path='/admin/food' component={Food}></Route>
+                                      <Route path='/admin/user' component={User}></Route>
+                                </Admin>
                             )
                         }}></Route>
-                        <Route path = '/login' component={Login}></Route>
+                        <Route path='/login' component={Login}></Route>
+
                     </Switch>
                  </HashRouter>
-             </App>
-         )
-     }
- }
- export default RootRouter
+            </App>
+            
+        ) 
+    }
+}
+export default RootRouter
