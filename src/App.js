361f87aa7b1da9cc0c63d5755d88e5862c8bce8c
biubiubiu-bei <you@example.com>
2019-09-17 17:37:36 +0800

    3454

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ function App(props) {
   return (
     <div className="App">
       {props.children}
+<<<<<<< HEAD
       {/* <Button></Button> */}
       hello world
       {/* <Button type="danger">
+=======
+      <Button type="danger">
+>>>>>>> 037187c5497e524d6f2fc4795a9a4127434e46e6
           hehe
-      </Button> */}
-      {/* <p>这里是APP</p> */}
+      </Button>
+      <p>这里是APP</p>
     </div>
   );
 }
