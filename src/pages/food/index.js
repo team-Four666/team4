0c211ee1a12c7a231654b470707805baed9e6776
biubiubiu-bei <you@example.com>
2019-09-20 14:13:56 +0800

    base

--- src/pages/food/index.js
+++ src/pages/food/index.js
@@ -1,13 +1,16 @@
 import React,{Component} from 'react'
-import {Card,Table,Button} from 'antd'
+import {Card,Table,Button,Pagination} from 'antd'
 // import foodDate from './foodDate'
-
 import './index.less'
+const qs = require('qs');
 class Food extends Component{
     constructor(){
         super()
         this.state={
-            dataSource:[]
+            dataSource:[],
+            page:1,
+            pageSize:3,
+            total:10
                       
         }
     }
@@ -16,14 +19,14 @@ class Food extends Component{
             title: '名称  ',//显示内容
             dataIndex: 'name',//下标
             key: 'name',//对应每一条数据的key值
-            width:150,
+            width:200,
             fixed:'left'
           },
           {
             title: '类型',
             dataIndex: 'foodtype',
             key: 'foodtype',
-            width:100,
+            width:200,
           },
           {
             title: '图片',
@@ -31,20 +34,20 @@ class Food extends Component{
             key: 'imgPath',
             width:200,
             render(data){
-                return(<img width='80px' src='http://img.bimg.126.net/photo/_lwWX4iDpsnriYFNG89HDA==/3975834046054828912.jpg'/>)
+                return(<img width='80px' src='http://n.sinaimg.cn/sinacn15/224/w640h384/20180520/1b6d-haturft2415067.jpg'/>)
             }
           },
           {
             title: '描述',
             dataIndex: 'desc',
             key: 'desc',
-            width:200
+            width:300
           },
           {
             title: '价格',
             dataIndex: 'price',
             key: 'price',
-            width:100,
+            width:200,
           },
           {
             title: '操作',
@@ -68,38 +71,36 @@ class Food extends Component{
     //     },500)
         
     // }
-    initData=()=>{
-        this.$axios.post('/hehe/admin/food/findByTypePage',{page:1})
-                           
+    pageChange=(page,pageSize)=>{
+        // console.log('页码改变',page,pageSize)
+        this.initData(page,this.state.pageSize)
+    }
+
+    initData=(page,pageSize)=>{ 
+        this.$axios.post('/hehe/admin/food/findByTypePage',qs.stringify({page:page,pageSize:pageSize}))                    
         .then((data)=>{
             console.log(data)
                 this.setState({dataSource:data.list})  
         })
     }
     componentDidMount(){
-        this.initData()
+        let {page,pageSize}=this.state
+        this.initData(page,pageSize)
     }
 
-    // initData=()=>{
-    //     this.$axios.post('/hehe/admin/food/findByTypePage',{page:1,pageSize:4,})
-    //     .then((data)=>{
-    //         console.log(data)
-    //     })
-    // }
-    // componentDidMount(){
-    //     this.initData()
-    // }
-
     render(){
+        let {total,pageSize}=this.state
         return( 
             <Card className='food-container'>
                 <Table dataSource={this.state.dataSource} 
                        className='test'
                        columns={this.columns} 
-                       scroll={{x:1000,y:400}}
+                       scroll={{x:1500,y:400}}
+                       pagination={false}
                        />
+                <Pagination simple defaultCurrent={1} total={total} pageSize={pageSize} onChange={this.pageChange}/>
             </Card> 
         )
-    } 
+    }  
 }
 export default Food
